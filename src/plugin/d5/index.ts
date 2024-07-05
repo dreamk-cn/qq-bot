85cf2519c81b02cb10d0f400c764ab462e50e504
dreamk <dreamk@qq.com>
2024-07-05 15:16:05 +0800

    feat: d5添加定时任务

--- src/plugin/d5/index.ts
+++ src/plugin/d5/index.ts
@@ -1,5 +1,6 @@
 import { Message, Plugin } from '../../core/types';
-import { QQClient } from '../../core/qq-client';
+import qClient, { QQClient } from '../../core/qq-client';
+import Cron from 'node-cron';
 
 const Survivor: string[] = [ '律师', '"囚徒"', '医生', '"小女孩"', '啦啦队员', '调香师', '佣兵', '"心理学家"'];
 const Hunter: string[] = [ '厂长', '鹿头', '小丑', '杰克', '蜘蛛', '宿伞之魂'];
@@ -12,18 +13,27 @@ function getRandomElement(arr: string[]) {
 }
 
 export default class D5 implements Plugin {
-  name = '第五人格！';
+  name = 'D5';
 
   priority = 90;
 
+  crons: Cron.ScheduledTask[] = [];
+
+  constructor() {
+    this.crons.push(Cron.schedule('* * 12,19 * * *', () => {
+      console.log('identify 5 cron!');
+      qClient.sendPrivateMessage({ message: '时间经不起等待，第五人格启动!', userId: 1633051172 });
+    }));
+  }
+
   async handle(question: Message, qqClient: QQClient): Promise<boolean> {
-    console.log('吊5插件开始处理消息');
     return new Promise(async (resolve) => {
       const content = qqClient.formateChatWithBot(question.raw_message);
       if (!qqClient.isChatWithBot(question)) {
         return resolve(false);
       }
-      if (!qqClient.isCommand(content, '随机监管者' || !qqClient.isCommand(content, '随机求生者'))) {
+      if (!qqClient.isCommand(content, '随机监管者' ||
+        !qqClient.isCommand(content, '随机求生者'))) {
         return resolve(false);
       }
       try {
