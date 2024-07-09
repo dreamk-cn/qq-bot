6de96565ccf27ed2bb151ab442b92530c7f19e75
dreamk <dreamk@qq.com>
2024-07-09 09:11:02 +0800

    feat: 更换指令/d -> d

--- src/plugin/dice/index.ts
+++ src/plugin/dice/index.ts
@@ -4,7 +4,7 @@ import { onCommand } from '@/core/command-decorator';
 import { random } from '@/util/random';
 
 function extractDiceRoll(input: string): { x?: number, y?: number } {
-  const pattern = /^\/d\s+(\d+)-(\d+)$/;
+  const pattern = /^d\s+(\d+)-(\d+)$/;
   const match = input.match(pattern);
   
   if (match) {
@@ -23,10 +23,11 @@ function extractDiceRoll(input: string): { x?: number, y?: number } {
 export default class Dice {
   name = '超级骰子';
 
-  @onCommand('/d', () => true, [], 50, true)
+  @onCommand('d', () => true, [], 50, true)
   async handle(question: Message): Promise<boolean> {
     return new Promise(async (resolve) => {
       const content = qqClient.formatRawMessage(question.raw_message);
+      console.log(content);
       try {
         const { x = 6, y = 11 } = extractDiceRoll(content);
         let sum = 0;
@@ -37,14 +38,14 @@ export default class Dice {
           const num = random(1, x);
           if (isBest && num === x) isBest = true; else isBest = false;
           if (isLow && num === 1) isLow = true; else isLow = false;
-          if (y <= 10) {
+          if (y > 1 && y <= 10) {
             detail += `\n第${i + 1}个骰子的点数为${num}点。`;
           }
           sum += num;
         }
         isLow = isLow && y >= 10 && x >= 6;
         isBest = isBest && y >= 10 && x >= 6;
-        const response = `${isBest ? '天选之子！全都是最大点数\n' : ''}${isLow ? '霉运之子？全都是最小点数\n' : ''}你投出了${y}个${x}面的骰子，共计${sum}点。${detail.length ? detail : '骰子太多，就不展示细节啦~'}`;
+        const response = `${isBest ? '天选之子！全都是最大点数\n' : ''}${isLow ? '霉运之子？全都是最小点数\n' : ''}你投出了${y}个${x}面的骰子，共计${sum}点。${(detail.length) ? detail : (y > 10 ? '骰子太多，就不展示细节啦~' : '') }`;
         qqClient.sendMessage({ message: response, userId: question.user_id, groupId: question.message_type === 'group' ? question.group_id : 0 });
         resolve(true);
       } catch (error) {
@@ -54,7 +55,7 @@ export default class Dice {
     });
   }
 
-  @onCommand('/d help', () => true, [], 50, true)
+  @onCommand('d help', () => true, [], 50, true)
   async handleHelp(question: Message): Promise<boolean> {
     qqClient.sendMessage({
       message: '骰子功能使用说明...',
